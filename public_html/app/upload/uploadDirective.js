56a44cd043644b332ce7b146b0cdcd8b5a71d5ba
Craig Graham <craiggraham@Craigs-MacBook-Pro.local>
2016-12-08 13:20:10 -0500

    adding webcam functionality

--- public_html/app/upload/uploadDirective.js
+++ public_html/app/upload/uploadDirective.js
@@ -5,7 +5,27 @@
     angular.module('whiteboardwhere.upload')
     
     
+
     
+        /***************************************************************************************
+        * Redirect the user to the homepage
+        ****************************************************************************************/ 
+        .directive("takeSnapshot", ['$location', function($location){
+            return function(scope, element, attrs){
+                element.bind("click", function(){
+                    
+                    
+                    
+                    Webcam.snap( function(data_uri) {
+                        document.getElementById('picture').innerHTML = '<img src="'+data_uri+'"/>';
+                        
+                        document.getElementById("result-wrapper").style.display = "inherit";
+                    } );
+
+                    
+                });
+            };
+        }])
 
     
         ;
