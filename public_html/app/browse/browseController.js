beb7ede2835347297010a9eaab7b20f3ea74e984
Craig Graham <craiggraham@Craigs-MacBook-Pro.local>
2016-12-09 10:05:48 -0500

    template of browse page

--- public_html/app/browse/browseController.js
+++ public_html/app/browse/browseController.js
@@ -16,7 +16,7 @@
       *  Global Variables
       ********************************************************************************/
       $scope.title = "Browse";
-
+      $scope.images = ["https://d1avok0lzls2w.cloudfront.net/uploads/blog/52b890a8554da2.07186970.jpg", "https://durv.files.wordpress.com/2012/02/2012-02-02-13-40-52.jpg", "https://d1avok0lzls2w.cloudfront.net/uploads/blog/5631501c761db7.99287967.jpg", "http://agilitrix.com/wp-content/uploads/2010/05/Product-Backlog-Mgmt-Cycle.jpg"]
 
 
       
